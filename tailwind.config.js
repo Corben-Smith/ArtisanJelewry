80f419cc16cc7b5882617219fd9cf73da59eac61
Corben-Smith <corbensmiht782@gmail.com>
2024-07-31 16:34:50 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      darkbackground: '#A79277',
+      background: '#D1BB9E',
+      cream: '#EAD8C0',
+      light: '#FFF2E1'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
